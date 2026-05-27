aed403abad01bf845c11fb5696df43b626d96125
philip <philip@wiemermail.de>
2026-05-27 13:04:57 +0200

    Changes Radio_Button Group Logic and Submit Button Event

--- app/src/main/java/com/philip/papp_jsongsonparsing/MainActivity.java
+++ app/src/main/java/com/philip/papp_jsongsonparsing/MainActivity.java
@@ -1,6 +1,10 @@
 package com.philip.papp_jsongsonparsing;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -10,8 +14,24 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Button submitButton;
+        RadioGroup radioGroup;
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.main_window);
+
+        // Logic Submit Button
+        radioGroup = findViewById(R.id.idColorRadioGroup1);
+        submitButton = findViewById(R.id.idSubmitButton);
+        submitButton.setOnClickListener(v -> {
+            int selectedId = radioGroup.getCheckedRadioButtonId();
+            // Nichts ausgewählt
+            if (selectedId == -1) {
+                Toast.makeText(this, "Bitte ein Modul wählen!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            RadioButton clickedButton = findViewById(selectedId);
+        });
     }
 }
---
+++ app/src/main/java/com/philip/papp_jsongsonparsing/parser/GsonParser.java
@@ -0,0 +1,4 @@
+package com.philip.papp_jsongsonparsing.parser;
+
+public class GsonParser {
+}
